7820bf8b655f76ebb95c266528f9f382900aa82c
aron <114922035+aronpaco@users.noreply.github.com>
2024-03-29 20:36:42 +0200

    bugfix: driver on second squad has last shift

--- main.py
+++ main.py
@@ -46,26 +46,37 @@ def convert_time_in_minutes_to_formatted_time(time_in_minutes):
 # convert_time_in_minutes_to_formatted_time(580)
 
 i = start_time_in_minutes
+squad_count = 0
 for squad in squads:
     print()
     print("--- Next squad ---")
     number_of_members_per_squad = len(squad)
     patrol_length_per_squad = time_length / number_of_squads
-    patrol_length_per_duo = patrol_length_per_squad / number_of_members_per_squad * 2
-
+    # Having an odd number of soldiers will leave one free of duty, so it should not affect the length per duo:
+    patrol_length_per_duo = patrol_length_per_squad / (number_of_members_per_squad // 2)
     if time_length <= 60 * 6:
         print("Drivers will not have any duties")
     elif time_length - patrol_length_per_duo < 60 * 6:
         print("Drivers will not do a full length of patrol; the patrol_length_per_duo must be recalculated")
     else:
-        # rearranging the list so that the drivers will be first, ensuring continous sleep
-        for soldier in squad:
-            if soldier.find("(Driver)") != -1:
-                arranged_squad.append(soldier)
-        for soldier in squad:
-            if soldier.find("(Driver)") == -1:
-                arranged_squad.append(soldier)
-        arranged_squads.append(arranged_squad)
+        # rearranging the list so that the drivers will be first or last, ensuring continous sleep
+        if squad_count % 2:
+            for soldier in squad:
+                if soldier.find("(Driver)") == -1:
+                    arranged_squad.append(soldier)
+            arranged_squads.append(arranged_squad)
+            for soldier in squad:
+                if soldier.find("(Driver)") != -1:
+                    arranged_squad.append(soldier)
+        else:
+            for soldier in squad:
+                if soldier.find("(Driver)") != -1:
+                    arranged_squad.append(soldier)
+            for soldier in squad:
+                if soldier.find("(Driver)") == -1:
+                    arranged_squad.append(soldier)
+            arranged_squads.append(arranged_squad)
+
 
         
         for j in range(len(arranged_squad) // 2):
@@ -77,16 +88,9 @@ for squad in squads:
             i += int(patrol_length_per_duo)
             print(shift)
 
-
-#        for soldier1, soldier2 in zip(arranged_squad, arranged_squad[1:]):
-#            shift_time = convert_time_in_minutes_to_formatted_time(i)
-#            shift = [shift_time, soldier1, soldier2]
-#            i += int(patrol_length_per_duo)
-#            print(shift)
-
-
             
         arranged_squad = []
+        squad_count += 1
 
 # [time, soldier1, soldier2]
 
