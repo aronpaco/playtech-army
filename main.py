4e0d97fb1a125af5caa278df23d7d5475fbcb908
aron <114922035+aronpaco@users.noreply.github.com>
2024-03-29 14:43:06 +0200

    feat: calculate time_length in minutes

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ start_hour, start_minute = map(int, start_time.split(":"))
 end_hour, end_minute = map(int, end_time.split(":"))
 
 if start_hour > end_hour:
-    time_length = 24 - start_hour + end_hour
+    time_length = 60 * (24 - start_hour) - start_minute + 60 * end_hour + end_minute
 
 print(time_length)
 print(start_hour, start_minute)
